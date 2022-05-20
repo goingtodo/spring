f89629bec6c2fe709e4b86827434b7fba45a9f96
goingtodo <yabbbklz1@gmail.com>
2022-05-20 20:23:51 +0900

    constructor injection - use @autowired

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,13 +1,27 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
+	private FortuneService fortuneService;
+	
+	@Autowired
+	public TennisCoach(FortuneService theFortuenService) {
+		fortuneService = theFortuenService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice tour backhand volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return fortuneService.getFortune();
 	}
 
 }
