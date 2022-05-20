98076e45583f26e5abe23d27ed7e20080762efc6
goingtodo <yabbbklz1@gmail.com>
2022-05-20 20:48:06 +0900

    field injection

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -6,19 +6,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+	@Autowired
 	private FortuneService fortuneService;
 	
 	//define a default constructor
 	public TennisCoach() {
 		System.out.println(">>TennisCoach: inside default constructor");
 	}
-	
 
+	/*
 	@Autowired
 	public void doSomeCrazyStuff(FortuneService theFortuneService) {
 		System.out.println(">>TennisCoach: inside  doSomeCrazyStuff() method");
 		fortuneService = theFortuneService;
 	}
+	*/
 	
 	/*
 	@Autowired
